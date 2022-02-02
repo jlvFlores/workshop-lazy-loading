54f23c9880a7118acbe23091595106b5e2c90fa0
jluis <jluisvazquez.jv@gmail.com>
2022-02-01 18:41:32 -0600

    Intersection Observer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from './lazy';
 
 const maximum = 122;
 const minimum = 1;
 
-const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
 
 const createImageNode = () => {
-    const container = document.createElement('div')
+    const container = document.createElement('div');
     container.className = "p-4";
 
-    const imagen = document.createElement('img')
+    const imagen = document.createElement('img');
     imagen.className = "mx-auto";
-    imagen.width = '300'
+    imagen.width = '300';
     imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
  
     container.appendChild(imagen);
@@ -23,14 +20,15 @@ const createImageNode = () => {
 }
   
 const NuevaImagen = createImageNode();
-const mountNode = document.getElementById("images")
+const mountNode = document.getElementById("images");
  
-const addButton = document.querySelector('button');
+const addButton = document.querySelector("button");
 
 
 const addImage = () => {
     const newImage = createImageNode();
     mountNode.append(newImage);
+    registerImage(newImage);
 };
 
-addButton.addEventListener('click', addImage);
+addButton.addEventListener("click", addImage);
